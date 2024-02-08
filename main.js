7fca6f8681f96b7e70c3c4f7382e59858fd55aa5
Mostafa Naderi <javascript.github@gmail.com>
2024-02-08 13:21:39 +0330

    add more logs

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const path=require('path')
 const {autoUpdater}=require('electron-updater')
 const log=require('electron-log')
 log.transports.file.resolvePathFn = () => path.join("/Users/mostafanaderi/Desktop/list/Udemy - Electron JS Complete Guide With Projects 2022-9/3. Auto Update Electron Application/auto-update-electron/package.json", 'logs/main.log');
+log.log("Application version= "+app.getVersion())
 let win;
 function createWindow(){
   win=new BrowserWindow({width:300,height:400})
@@ -15,16 +16,33 @@ app.on('ready',()=>{
 })
 
 autoUpdater.on("update-available",()=>{
-  console.log("hello")
+  log.info("update available")
+
 })
 
 autoUpdater.on("checking-for-update",()=>{
-  console.log("checking-for-update")
+  log.info("checking for update")
+
 })
 
 autoUpdater.on("download-progress",()=>{
-  console.log("download-progress")
+  log.info("download progress")
 })
+
+autoUpdater.on("download-progress",(progress)=>{
+  log.info("\n\ndownload-progress")
+  log.info(progress)
+
+}) 
 autoUpdater.on("update-downloaded",()=>{
-  console.log("update-downloaded")
+  log.info("update downloaded")
 })
+autoUpdater.on("update-not-available",()=>{
+  log.info("update not available")
+
+})
+autoUpdater.on("error",(err)=>{
+  log.info("error in autoUpdater "+err)
+
+})
+
